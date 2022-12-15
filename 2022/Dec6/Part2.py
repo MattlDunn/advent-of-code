c482fd8141114c9f79693863a4bce4945baae2ad
Matt <mattldunn10@gmail.com>
2022-12-15 17:38:47 -0500

    December 6

---
+++ 2022/Dec6/Part2.py
@@ -0,0 +1,21 @@
+in_txt = open('Full.in', 'r')
+line = in_txt.readline().rstrip()
+
+index = 0
+unique_count = 0
+char_at_index = {}
+
+while index < len(line):
+    if unique_count == 14:
+        break
+
+    if line[index] in char_at_index:
+        index = char_at_index[line[index]] + 1
+        char_at_index = {}
+        unique_count = 0
+    else:
+        char_at_index[line[index]] = index
+        unique_count += 1
+        index += 1
+
+print(index)
